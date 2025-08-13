3d8a45d1ced522223d2f55d20fd119c2895b71ae
soso7194 <wons5215@naver.com>
2025-08-13 23:09:47 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cs
@@ -0,0 +1,8 @@
+            string[] i = {"11\nA B C D E F G H J L M"
+            ,"9\nA C E F G H I L M", "9\nA C E F G H I L M"
+            ,"9\nA B C E F G H L M", "8\nA C E F G H L M"
+            ,"8\nA C E F G H L M", "8\nA C E F G H L M"
+            ,"8\nA C E F G H L M", "8\nA C E F G H L M"
+            ,"8\nA B C F G H L M"};
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(i[n - 1]);
