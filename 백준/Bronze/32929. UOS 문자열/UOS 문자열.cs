d1e2330cbb7ac6e86aea014d1ea26fd225bc67c9
soso7194 <wons5215@naver.com>
2025-08-13 22:23:49 +0900

    [Bronze V] Title: UOS 문자열, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.cs
@@ -0,0 +1,8 @@
+            int a = int.Parse(Console.ReadLine());
+
+            if(a % 3 == 1)
+                Console.WriteLine("U");
+            else if(a % 3 == 2)
+                Console.WriteLine("O");
+            else if(a % 3 == 0)
+                Console.WriteLine("S");
