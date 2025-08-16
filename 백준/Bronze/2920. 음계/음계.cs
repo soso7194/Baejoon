645435811fa5bd4a4286e21d9edbc1c983991696
soso7194 <wons5215@naver.com>
2025-08-16 21:34:42 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,7 @@
+            string input = Console.ReadLine();
+
+            if (input == "1 2 3 4 5 6 7 8")
+                Console.WriteLine("ascending");
+            else if (input == "8 7 6 5 4 3 2 1")
+                Console.WriteLine("descending");
+            else Console.WriteLine("mixed");
