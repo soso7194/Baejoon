8b73bfb5fbf7eb198e686fd50fd26708f1f6a7be
soso7194 <wons5215@naver.com>
2025-08-16 21:24:21 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 48 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,6 @@
+            for (int i = 0; i < 100; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == null) break;
+                Console.WriteLine(input);
+            }
