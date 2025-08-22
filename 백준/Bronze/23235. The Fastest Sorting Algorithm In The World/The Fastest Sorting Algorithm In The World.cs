c68b2c849fe4edd7971e6e18a7ecc44a2f0c94ff
soso7194 <wons5215@naver.com>
2025-08-23 00:40:34 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 52 ms, Memory: 5668 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cs
@@ -0,0 +1,8 @@
+            int i = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "0") break;
+                i++;
+                Console.WriteLine($"Case {i}: Sorting... done!");
+            }
