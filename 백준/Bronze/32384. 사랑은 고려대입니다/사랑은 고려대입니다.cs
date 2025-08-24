9a3afd4d01ddd75624bbdb944a6900f993be62d9
soso7194 <wons5215@naver.com>
2025-08-24 21:04:41 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cs
@@ -0,0 +1,9 @@
+            int a = int.Parse(Console.ReadLine());
+            string b = "LoveisKoreaUniversity";
+
+            for (int i = 0; i < a - 1; i++)
+            {
+                Console.Write(b);
+                Console.Write(' ');
+            }
+            Console.Write(b);
