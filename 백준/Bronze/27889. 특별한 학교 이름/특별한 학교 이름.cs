86b4b952a33a31bcce01057918003412a5365839
soso7194 <wons5215@naver.com>
2025-09-01 08:55:54 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 44 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cs
@@ -0,0 +1,10 @@
+            string n = Console.ReadLine();
+
+            if(n == "NLCS")
+                Console.WriteLine("North London Collegiate School");
+            else if (n == "BHA")
+                Console.WriteLine("Branksome Hall Asia");
+            else if (n == "KIS")
+                Console.WriteLine("Korea International School");
+            else
+                Console.WriteLine("St. Johnsbury Academy");
