31222d2250eaf9557d2836e6cd4016e179fef021
soso7194 <wons5215@naver.com>
2025-09-03 16:33:19 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 48 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cs
@@ -0,0 +1,9 @@
+            int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int n1 = 1 - n[0];
+            int n2 = 1 - n[1];
+            int n3 = 2 - n[2];
+            int n4 = 2 - n[3];
+            int n5 = 2 - n[4];
+            int n6 = 8 - n[5];
+
+            Console.WriteLine($"{n1} {n2} {n3} {n4} {n5} {n6}");
