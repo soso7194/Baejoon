8e6876838451cafabb40fc0c11d1663f3a84a3df
soso7194 <wons5215@naver.com>
2025-09-09 14:40:05 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,10 @@
+            string n = Console.ReadLine();
+
+            if(n == "SONGDO")
+                Console.WriteLine("HIGHSCHOOL");
+            if(n == "CODE")
+                Console.WriteLine("MASTER");
+            if(n == "2023")
+                Console.WriteLine("0611");
+            if(n == "ALGORITHM")
+                Console.WriteLine("CONTEST");
