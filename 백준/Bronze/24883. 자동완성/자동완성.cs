764d7dc8f18658f7012fe8204e71b44a373ca6af
soso7194 <wons5215@naver.com>
2025-09-15 09:07:30 +0900

    [Bronze V] Title: 자동완성, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cs
@@ -0,0 +1,10 @@
+                string n = Console.ReadLine();
+
+                if(n == "N" || n == "n")
+                {
+                    Console.WriteLine("Naver D2");
+                }
+                else
+                {
+                    Console.WriteLine("Naver Whale");
+                }
