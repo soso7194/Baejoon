d4fe184cf11e780c6488c513f4c25cba7fc4aefe
soso7194 <2408shin@naver.com>
2026-01-05 22:48:08 +0900

    [Bronze V] Title: Copier, Time: 52 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cs
@@ -0,0 +1,6 @@
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 0; i < n; i++)
+        {
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{m} {m}");
+        }
