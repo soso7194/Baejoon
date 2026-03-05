6da7b4042cc2bb88f5c18bc5545aedd09bd01c1b
soso7194 <2408shin@naver.com>
2026-03-05 19:29:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cs
@@ -0,0 +1,24 @@
+        int n = int.Parse(Console.ReadLine());
+        long[] arr = { 0, 1 };
+
+        if (n == 0)
+        {
+            Console.WriteLine(arr[0]);
+        }
+        else if (n == 1)
+        {
+            Console.WriteLine(arr[1]);
+        }
+        else
+        {
+            for(int i = 0; i < n - 1; i++)
+            {
+                long add = 0;
+                long num = arr[1];
+
+                add = arr[0] + arr[1];
+                arr[1] = add;
+                arr[0] = num;
+            }
+            Console.WriteLine(arr[1]);
+        }
