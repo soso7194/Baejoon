c9ba44a60a1c1d86d8b21b30355679449f043efb
soso7194 <2408shin@naver.com>
2026-03-23 23:08:32 +0900

    [Bronze V] Title: 채플이 너무해, Time: 44 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cs
@@ -0,0 +1,9 @@
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 6)
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Oh My God!");
+            }
